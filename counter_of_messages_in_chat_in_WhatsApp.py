8f3e9934fd839702ae2e232df3143128391bc666
Maximilian Schön <maxleonschoen@gmail.com>
2022-10-15 13:00:05 +0200

    add assignment on count by participant

--- counter_of_messages_in_chat_in_WhatsApp.py
+++ counter_of_messages_in_chat_in_WhatsApp.py
@@ -3,7 +3,16 @@ import re
 file = open(sys.argv[1], "r", encoding='utf-8')
 lines = file.readlines()
 count_of_messages = 0
+IDs = set()
+counts_of_messages_by_ID = {}
+separator_between_time_and_ID = " - "
 for line in lines:
-    if re.match("[1-9]|1[0-2]/[1-9]|[12][0-9]|3[01]/\d{2} [0-2][0-9]:[0-5][0-9] - ", line):
+    signature = re.match("(?=([1-9]|(1[0-2]))/([1-9]|([12][0-9])|(3[01]))/\d{2}, (([01][0-9])|(2[0-3])):[0-5][0-9] - ).+?(?=: .+)", line)
+    if signature:
+        ID = signature.group().replace(re.match("([1-9]|(1[0-2]))/([1-9]|([12][0-9])|(3[01]))/\d{2}, (([01][0-9])|(2[0-3])):[0-5][0-9]" + separator_between_time_and_ID, line).group(), '')
         count_of_messages += 1
+        IDs.add(ID)
+        counts_of_messages_by_ID[ID] = 1 if not ID in counts_of_messages_by_ID.keys() else counts_of_messages_by_ID[ID] + 1
 print(f"{count_of_messages} messages")
+for ID in counts_of_messages_by_ID.keys():
+    print(f"{ID}: {counts_of_messages_by_ID[ID]} messages")

8c970f50681414d76f08a2663fd078b63fa61a73
Maximilian Schön <maxleonschoen@gmail.com>
2022-10-15 13:21:38 +0200

    clean

--- counter_of_messages_in_chat_in_WhatsApp.py
+++ counter_of_messages_in_chat_in_WhatsApp.py
@@ -6,10 +6,11 @@ count_of_messages = 0
 IDs = set()
 counts_of_messages_by_ID = {}
 separator_between_time_and_ID = " - "
+pattern_for_time = r"([1-9]|(1[0-2]))/|\.([1-9]|([12][0-9])|(3[01]))/\.\d{2}, (([01][0-9])|(2[0-3])):[0-5][0-9]"
 for line in lines:
-    signature = re.match("(?=([1-9]|(1[0-2]))/([1-9]|([12][0-9])|(3[01]))/\d{2}, (([01][0-9])|(2[0-3])):[0-5][0-9] - ).+?(?=: .+)", line)
+    signature = re.match(f"(?={pattern_for_time}{separator_between_time_and_ID}).+?(?=: .+)", line)
     if signature:
-        ID = signature.group().replace(re.match("([1-9]|(1[0-2]))/([1-9]|([12][0-9])|(3[01]))/\d{2}, (([01][0-9])|(2[0-3])):[0-5][0-9]" + separator_between_time_and_ID, line).group(), '')
+        ID = signature.group().replace(re.match(pattern_for_time + separator_between_time_and_ID, line).group(), '')
         count_of_messages += 1
         IDs.add(ID)
         counts_of_messages_by_ID[ID] = 1 if not ID in counts_of_messages_by_ID.keys() else counts_of_messages_by_ID[ID] + 1

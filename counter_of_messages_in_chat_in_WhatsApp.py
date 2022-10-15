cf1c9869e8f7dfccacaa9622d644fde9d282a693
Maximilian Schön <maxleonschoen@gmail.com>
2022-10-15 12:57:49 +0200

    rename variable

--- counter_of_messages_in_chat_in_WhatsApp.py
+++ counter_of_messages_in_chat_in_WhatsApp.py
@@ -1,7 +1,7 @@
 import sys
 import re
-f = open(sys.argv[1], "r", encoding='utf-8')
-lines = f.readlines()
+file = open(sys.argv[1], "r", encoding='utf-8')
+lines = file.readlines()
 count_of_messages = 0
 for line in lines:
     if re.match("[1-9]|1[0-2]/[1-9]|[12][0-9]|3[01]/\d{2} [0-2][0-9]:[0-5][0-9] - ", line):
